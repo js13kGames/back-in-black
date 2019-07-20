fda65a685c7742d5a7d593f98dbee555fcbcf809
jameswilddev <jameswilddev@outlook.com>
2019-07-20 18:04:13 +0100

    Implemented CI and CLI.

--- src/pipeline/ci.ts
+++ src/pipeline/ci.ts
@@ -1,1 +1,33 @@
-console.log(`CI Placeholder.`)
+import * as recursiveReaddir from "recursive-readdir"
+import isMonitored from "./files/is-monitored"
+import Diff from "./files/diff"
+import plan from "./planning/plan"
+
+async function program(): Promise<void> {
+  console.log(`Searching for files...`)
+  const files = (await recursiveReaddir(`src`)).filter(isMonitored)
+  console.log(`Planning...`)
+  const step = plan(new Diff(files, [], [], []), true, false)
+  console.log(`Executing plan...`)
+  await step.executePerActionStep(async (step, execute) => {
+    const description = step.getSingleLineDescription()
+    console.log(`Starting step "${description}"...`)
+    try {
+      await execute()
+    } catch (e) {
+      console.error(`Error in step "${description}":`)
+      console.error(e)
+      process.exit(1)
+    }
+    console.log(`Completed step "${description}".`)
+  })
+  console.log(`Done.`)
+}
+
+program().then(
+  () => { },
+  (error: any) => {
+    console.error(error)
+    process.exit(1)
+  }
+)
--- src/pipeline/cli.ts
+++ src/pipeline/cli.ts
@@ -1,1 +1,58 @@
-console.log(`CLI Placeholder.`)
+import * as progress from "progress"
+import * as types from "./types"
+import watch from "./files/watch"
+import diffFileVersions from "./files/diff-file-versions"
+import plan from "./planning/plan"
+
+async function program(): Promise<void> {
+  let previousFileVersions: types.FileVersions = {}
+  let firstRun = true
+  await watch(async nextFileVersions => {
+    const step = plan(diffFileVersions(previousFileVersions, nextFileVersions), firstRun, true)
+    previousFileVersions = nextFileVersions
+    firstRun = false
+    let totalActions = 0
+    await step.executePerActionStep(async (step, execute) => { totalActions++ })
+    if (totalActions > 0) {
+      const bar = new progress(
+        `:bar :current/:total (:percent) :etas :message`,
+        {
+          width: 80,
+          total: totalActions + 1,
+        }
+      )
+
+      bar.render({ message: `Starting...` })
+
+      try {
+        await step.executePerActionStep(async (step, execute) => {
+          const description = step.getSingleLineDescription()
+          bar.render({ message: description })
+          try {
+            await execute()
+          } catch (e) {
+            console.error()
+            console.error(`Error in step "${description}":`)
+            throw e
+          }
+          bar.tick()
+        })
+      } catch (e) {
+        console.error()
+        console.error(e)
+      }
+
+      bar.tick({ message: `Done.` })
+    } else {
+      console.log(`Nothing to do.`)
+    }
+  })
+}
+
+program().then(
+  () => { },
+  (error: any) => {
+    console.error(error)
+    process.exit(1)
+  }
+)
