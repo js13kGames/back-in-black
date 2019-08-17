4c3056b9814f0c31575d84936f8e29c88d082535
jameswilddev <jameswilddev@outlook.com>
2019-08-17 12:37:48 +0100

    Simplified the first level.

--- src/games/back-in-black/src/levels/get-in-get-it-get-out.ts
+++ src/games/back-in-black/src/levels/get-in-get-it-get-out.ts
@@ -2,18 +2,6 @@ const getInGetItGetOut: Level = {
   name: `get in,get it,get out`,
   rooms: [{
     type: `mcguffin`,
-    x: 1,
-    y: 3
-  }, {
-    type: `empty`,
-    x: 2,
-    y: 3
-  }, {
-    type: `empty`,
-    x: 2,
-    y: 2
-  }, {
-    type: `empty`,
     x: 3,
     y: 2
   }, {
@@ -32,73 +20,31 @@ const getInGetItGetOut: Level = {
     type: `empty`,
     x: 5,
     y: 4
-  }, {
-    type: `empty`,
-    x: 6,
-    y: 4
-  }, {
-    type: `empty`,
-    x: 6,
-    y: 3
-  }, {
-    type: `empty`,
-    x: 7,
-    y: 3
   }],
   corridors: [{
     type: `empty`,
-    x: 2,
-    y: 3,
-    facing: `west`
-  }, {
-    type: `empty`,
-    x: 2,
-    y: 2,
-    facing: `south`
-  }, {
-    type: `empty`,
     x: 3,
     y: 2,
-    facing: `west`
+    facing: `east`
   }, {
     type: `empty`,
     x: 4,
     y: 2,
-    facing: `west`
+    facing: `south`
   }, {
     type: `empty`,
     x: 4,
-    y: 3,
-    facing: `north`
-  }, {
-    type: `empty`,
-    x: 4,
-    y: 4,
-    facing: `north`
-  }, {
-    type: `empty`,
-    x: 5,
-    y: 4,
-    facing: `west`
-  }, {
-    type: `empty`,
-    x: 6,
-    y: 4,
-    facing: `west`
-  }, {
-    type: `empty`,
-    x: 6,
     y: 3,
     facing: `south`
   }, {
     type: `empty`,
-    x: 7,
-    y: 3,
-    facing: `west`
+    x: 4,
+    y: 4,
+    facing: `east`
   }, {
     type: `goal`,
-    x: 7,
-    y: 3,
+    x: 5,
+    y: 4,
     facing: `east`
   }]
 }
