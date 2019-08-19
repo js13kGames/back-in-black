d7cf2cc088c127e3107081c4b1a0f6ac75acb90f
jameswilddev <jameswilddev@outlook.com>
2019-08-19 16:58:44 +0100

    Added a music track.

---
+++ src/games/back-in-black/src/audio/music/bass.ts
@@ -0,0 +1,49 @@
+let bassOscillatorA: OscillatorNode
+let bassOscillatorB: OscillatorNode
+let bassLowpass: BiquadFilterNode
+let bassGain: GainNode
+
+function setUpBass(): void {
+  bassOscillatorA = audioContext.createOscillator()
+  bassOscillatorA.type = `sawtooth`
+  bassOscillatorA.detune.value = -6
+
+  bassOscillatorB = audioContext.createOscillator()
+  bassOscillatorB.detune.value = 6
+  bassOscillatorB.type = `sawtooth`
+
+  const channelMerger = audioContext.createChannelMerger(2)
+
+  bassLowpass = audioContext.createBiquadFilter()
+  bassLowpass.type = `lowpass`
+  bassLowpass.Q.value = 8
+
+  bassGain = audioContext.createGain()
+  bassGain.gain.value = 0
+
+  bassOscillatorA.connect(channelMerger, undefined, 0)
+  bassOscillatorB.connect(channelMerger, undefined, 1)
+  channelMerger.connect(bassLowpass)
+  bassLowpass.connect(bassGain)
+  bassGain.connect(audioContext.destination)
+
+  bassOscillatorA.start(0)
+  bassOscillatorB.start(0)
+}
+
+function bassOn(at: number, note: number): void {
+  const startTime = audioTime(at)
+  note += 3
+  const frequency = 55 * Math.pow(2, note / 12)
+  bassOscillatorA.frequency.setValueAtTime(frequency, startTime)
+  bassOscillatorB.frequency.setValueAtTime(frequency, startTime)
+  bassLowpass.frequency.setValueAtTime(2000, startTime)
+  bassLowpass.frequency.linearRampToValueAtTime(500, startTime + 0.1)
+  bassGain.gain.setValueAtTime(1, startTime)
+  bassGain.gain.linearRampToValueAtTime(0.5, startTime + 0.15)
+}
+
+function bassOff(at: number): void {
+  const startTime = audioTime(at)
+  bassGain.gain.setValueAtTime(0, startTime)
+}
---
+++ src/games/back-in-black/src/audio/music/snare.ts
@@ -0,0 +1,38 @@
+let snareGain: GainNode
+let snareBandpass: BiquadFilterNode
+let snareOscillator: OscillatorNode
+let snareOscillatorGain: GainNode
+
+function setUpSnare(): void {
+  snareGain = audioContext.createGain()
+  snareGain.gain.value = 0
+  snareBandpass = audioContext.createBiquadFilter()
+  snareBandpass.type = `bandpass`
+  snareBandpass.frequency.value = 1800
+  snareBandpass.Q.value = 0.9
+  whiteNoise.connect(snareGain)
+  snareGain.connect(snareBandpass)
+  snareBandpass.connect(audioContext.destination)
+
+  snareOscillator = audioContext.createOscillator()
+  snareOscillator.type = `triangle`
+  snareOscillatorGain = audioContext.createGain()
+  snareOscillatorGain.gain.value = 0
+  snareOscillator.connect(snareOscillatorGain)
+  snareOscillatorGain.connect(audioContext.destination)
+  snareOscillator.start(0)
+}
+
+function snare(at: number): void {
+  const startTime = audioTime(at)
+  snareGain.gain.setValueAtTime(1, startTime)
+  snareBandpass.frequency.setValueAtTime(2000, startTime)
+  snareOscillatorGain.gain.setValueAtTime(1, startTime)
+  snareOscillator.frequency.setValueAtTime(100, startTime)
+
+  snareGain.gain.linearRampToValueAtTime(0.4, startTime + 0.1)
+  snareGain.gain.linearRampToValueAtTime(0, startTime + 0.2)
+  snareBandpass.frequency.linearRampToValueAtTime(1000, startTime + 0.15)
+  snareOscillatorGain.gain.linearRampToValueAtTime(0, startTime + 0.08)
+  snareOscillator.frequency.linearRampToValueAtTime(0, startTime + 0.08)
+}
