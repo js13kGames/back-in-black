d17e4ffcfda49cb1c5adb823cf28b49ef4f4f024
jameswilddev <jameswilddev@outlook.com>
2019-08-23 22:51:07 +0100

    Merged placeholder files (#60).

--- src/engine/src/placeholders.d.ts
+++ src/engine/src/placeholders.d.ts
@@ -1,1 +1,11 @@
 declare const gameName: string
+
+type State = `STATE PLACEHOLDER`
+
+declare function initial(): State
+
+declare const version: number
+
+declare function audioReady(): void
+
+declare function render(): void
