9926a87b62194766b74e9ba282c943199df5690b
jameswilddev <jameswilddev@outlook.com>
2019-08-25 09:02:09 +0100

    Shifted rooms to accommodate the adjusted centering (#60).

--- src/games/back-in-black/src/game/levels/adoorable.ts
+++ src/games/back-in-black/src/game/levels/adoorable.ts
@@ -2,40 +2,40 @@ const adoorable: Level = {
   name: `adoorable`,
   rooms: [{
     type: `mcguffin`,
-    x: 3,
-    y: 3
+    x: -1,
+    y: 0
   }, {
     type: `empty`,
-    x: 5,
-    y: 3
+    x: 1,
+    y: 0
   }, {
     type: `switch`,
-    x: 5,
-    y: 2
+    x: 1,
+    y: -1
   }, {
     type: `empty`,
-    x: 4,
-    y: 3
+    x: 0,
+    y: 0
   }],
   corridors: [{
     type: `empty`,
-    x: 5,
-    y: 3,
+    x: 1,
+    y: 0,
     facing: `north`
   }, {
     type: `empty`,
-    x: 4,
-    y: 3,
+    x: 0,
+    y: 0,
     facing: `west`
   }, {
     type: `closedDoor`,
-    x: 5,
-    y: 3,
+    x: 1,
+    y: 0,
     facing: `west`
   }, {
     type: `goal`,
-    x: 5,
-    y: 3,
+    x: 1,
+    y: 0,
     facing: `east`
   }]
 }
