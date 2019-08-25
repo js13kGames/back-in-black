94f7721c1a10bc73b7269c9b08b468f068b2dbdf
jameswilddev <jameswilddev@outlook.com>
2019-08-25 09:33:13 +0100

    Added a means to implement switches (#60).

--- src/games/back-in-black/src/game/render.ts
+++ src/games/back-in-black/src/game/render.ts
@@ -4,19 +4,32 @@ function renderNonInteractiveGame(
 ): () => void {
   const level = levels[gamePhase.level]
 
+  const changeOnSwitch: {
+    readonly parent: EngineAnimation
+    readonly hide: EngineAnimation
+    readonly show: EngineSpritesSvg
+  }[] = []
+
   for (const room of level.rooms) {
     const roomGroup = group(parent)
     translate(roomGroup, room.x * roomSpacing, room.y * roomSpacing)
 
     switch (room.type) {
-      case `empty`:
+      case `empty`: {
         sprite(roomGroup, game_room_empty_svg)
-        break
+      } break
       case `mcguffin`:
         sprite(roomGroup, game_room_mcguffin_a_svg)
         break
       case `switch`:
-        sprite(roomGroup, game_room_switch_a_svg)
+        changeOnSwitch.push({
+          parent: roomGroup,
+          hide: sprite(
+            roomGroup,
+            gamePhase.switch == `a` ? game_room_switch_a_svg : game_room_switch_b_svg
+          ),
+          show: gamePhase.switch == `a` ? game_room_switch_b_svg : game_room_switch_a_svg,
+        })
         break
       default:
         throw null
@@ -39,10 +52,24 @@ function renderNonInteractiveGame(
         sprite(corridorGroup, game_corridor_stairs_svg)
         break
       case `openDoor`:
-        sprite(corridorGroup, game_corridor_door_open_svg)
+        changeOnSwitch.push({
+          parent: corridorGroup,
+          hide: sprite(
+            corridorGroup,
+            gamePhase.switch == `a` ? game_corridor_door_open_svg : game_corridor_door_closed_svg
+          ),
+          show: gamePhase.switch == `a` ? game_corridor_door_closed_svg : game_corridor_door_open_svg,
+        })
         break
       case `closedDoor`:
-        sprite(corridorGroup, game_corridor_door_closed_svg)
+        changeOnSwitch.push({
+          parent: corridorGroup,
+          hide: sprite(
+            corridorGroup,
+            gamePhase.switch == `a` ? game_corridor_door_closed_svg : game_corridor_door_open_svg
+          ),
+          show: gamePhase.switch == `a` ? game_corridor_door_open_svg : game_corridor_door_closed_svg,
+        })
         break
       case `goal`:
         sprite(corridorGroup, game_corridor_goal_closed_svg)
