eec9a6902e7389418cccd4adc7e8e3e10d51c1ef
jameswilddev <jameswilddev@outlook.com>
2019-08-26 08:50:43 +0100

    Reimplemented text writing (#60).

---
+++ src/games/back-in-black/src/font/write.ts
@@ -0,0 +1,14 @@
+function write(
+  parent: EngineViewport | EngineAnimation,
+  text: string
+): void {
+  let x = -7.5 * (text.length - 1)
+  for (let i = 0; i < text.length; i++) {
+    const character = text.charAt(i)
+    if (character != ` `) {
+      const characterSprite = sprite(parent, font[character])
+      translateX(characterSprite, x)
+    }
+    x += 15
+  }
+}
