f3096e795863da9e31523406a37473741c08abce
jameswilddev <jameswilddev@outlook.com>
2019-08-29 21:41:42 +0100

    Fixed menus disappearring immediately on dismissing them (#60).

--- src/games/back-in-black/src/game/hud/menu.ts
+++ src/games/back-in-black/src/game/hud/menu.ts
@@ -9,29 +9,34 @@ type Menu = {
 function renderNonInteractiveMenu(
   parent: EngineViewport | EngineAnimation,
   menu: Menu,
+  enter: boolean,
 ): void {
   let y = menu.options.length * -16
   const title = group(parent)
   write(title, menu.title)
   translateY(title, y)
-  hide(title)
-  scaleX(title, 0.1)
-  easeOut(title)
-  elapse(120)
-  show(title)
-  scaleX(title, 10)
+  if (enter) {
+    hide(title)
+    scaleX(title, 0.1)
+    easeOut(title)
+    elapse(120)
+    show(title)
+    scaleX(title, 10)
+  }
   y += 32
   for (const option of menu.options) {
     const optionGroup = group(parent)
     translateY(optionGroup, y)
     sprite(optionGroup, game_hud_button_svg)
     write(optionGroup, option.label)
-    hide(optionGroup)
-    scaleX(optionGroup, 0.1)
-    easeOut(optionGroup)
-    elapse(120)
-    show(optionGroup)
-    scaleX(optionGroup, 10)
+    if (enter) {
+      hide(optionGroup)
+      scaleX(optionGroup, 0.1)
+      easeOut(optionGroup)
+      elapse(120)
+      show(optionGroup)
+      scaleX(optionGroup, 10)
+    }
     y += 32
   }
 }
--- src/games/back-in-black/src/game/render.ts
+++ src/games/back-in-black/src/game/render.ts
@@ -65,196 +65,201 @@ function renderNonInteractiveGame(
 ): () => void {
   const level = levels[mode.level]
 
-  const levelName = group(parent)
-  translateY(levelName, 15 - halfSafeAreaHeightVirtualPixels)
-  write(levelName, level.name)
-
-  let mcguffinX = 0
-  let mcguffinY = 0
-  for (const room of level.rooms) {
-    if (room.type == `mcguffin`) {
-      mcguffinX = room.x
-      mcguffinY = room.y
-      break
-    }
-  }
-
-  const changeOnSwitch: {
-    readonly parent: EngineAnimation
-    readonly hide: EngineAnimation
-    readonly show: EngineSpritesSvg
-  }[] = []
-
-  const hideWhenTaken: {
-    readonly hide: EngineAnimation
-    readonly distance: number
-  }[] = []
-
-  if (mode.state == `initial` || mode.animation == `take`) {
-    const switchState = mode.switch == (mode.animation == `switch` ? `b` : `a`)
-
-    for (const room of level.rooms) {
-      const roomGroup = group(parent)
-      translate(roomGroup, room.x * roomSpacing, room.y * roomSpacing)
-      hideWhenTaken.push({
-        hide: roomGroup,
-        distance: distanceSquared(room.x, room.y, mcguffinX, mcguffinY),
-      })
-
-      switch (room.type) {
-        case `empty`: {
-          sprite(roomGroup, game_room_empty_svg)
-        } break
-        case `mcguffin`:
-          sprite(roomGroup, game_room_mcguffin_a_svg)
-          break
-        case `switch`:
-          changeOnSwitch.push({
-            parent: roomGroup,
-            hide: sprite(
-              roomGroup,
-              switchState ? game_room_switch_a_svg : game_room_switch_b_svg
-            ),
-            show: switchState ? game_room_switch_b_svg : game_room_switch_a_svg,
-          })
-          break
-        default:
-          throw null
-      }
-    }
-
-    for (const corridor of level.corridors) {
-      const corridorGroup = group(parent)
-      translate(corridorGroup, corridor.x * roomSpacing, corridor.y * roomSpacing)
-      rotate(corridorGroup, facingDegrees[corridor.facing])
-      hideWhenTaken.push({
-        hide: corridorGroup,
-        distance: Math.min(
-          distanceSquared(corridor.x, corridor.y, mcguffinX, mcguffinY),
-          distanceSquared(corridor.x + facingX[corridor.facing], corridor.y + facingY[corridor.facing], mcguffinX, mcguffinY)
-        )
-      })
-
-      switch (corridor.type) {
-        case `empty`:
-          sprite(corridorGroup, game_corridor_empty_svg)
-          break
-        case `ledge`:
-          sprite(corridorGroup, game_corridor_ledge_svg)
-          break
-        case `stairs`:
-          sprite(corridorGroup, game_corridor_stairs_svg)
-          break
-        case `openDoor`:
-          changeOnSwitch.push({
-            parent: corridorGroup,
-            hide: sprite(
-              corridorGroup,
-              switchState ? game_corridor_door_open_svg : game_corridor_door_closed_svg
-            ),
-            show: switchState ? game_corridor_door_closed_svg : game_corridor_door_open_svg,
-          })
-          break
-        case `closedDoor`:
-          changeOnSwitch.push({
-            parent: corridorGroup,
-            hide: sprite(
-              corridorGroup,
-              switchState ? game_corridor_door_closed_svg : game_corridor_door_open_svg
-            ),
-            show: switchState ? game_corridor_door_open_svg : game_corridor_door_closed_svg,
-          })
-          break
-        case `goal`:
-          sprite(corridorGroup, game_corridor_goal_closed_svg)
-          break
-        default:
-          throw null
-      }
-    }
-  }
-
-  const playerGroup = group(parent)
-  translate(playerGroup, mode.x * roomSpacing, mode.y * roomSpacing)
-  rotate(playerGroup, facingDegrees[mode.facing])
-
-  const playerIdleA = sprite(playerGroup, game_player_idle_a_lit_svg)
-  const playerIdleB = sprite(playerGroup, game_player_idle_b_lit_svg)
-  const playerWalk = sprite(playerGroup, game_player_walk_lit_svg)
-
-  hide(playerIdleB)
-
-  if (mode.animation) {
-    hide(playerIdleA)
-    translateX(playerGroup, -roomSpacing)
+  if (mode.state == `won` && !mode.animation) {
+    renderNonInteractiveMenu(parent, postGameMenu(mode), false)
+    return () => { }
   } else {
-    hide(playerWalk)
-  }
+    const levelName = group(parent)
+    translateY(levelName, 15 - halfSafeAreaHeightVirtualPixels)
+    write(levelName, level.name)
 
-  return () => {
+    let mcguffinX = 0
+    let mcguffinY = 0
+    for (const room of level.rooms) {
+      if (room.type == `mcguffin`) {
+        mcguffinX = room.x
+        mcguffinY = room.y
+        break
+      }
+    }
+
+    const changeOnSwitch: {
+      readonly parent: EngineAnimation
+      readonly hide: EngineAnimation
+      readonly show: EngineSpritesSvg
+    }[] = []
+
+    const hideWhenTaken: {
+      readonly hide: EngineAnimation
+      readonly distance: number
+    }[] = []
+
+    if (mode.state == `initial` || mode.animation == `take`) {
+      const switchState = mode.switch == (mode.animation == `switch` ? `b` : `a`)
+
+      for (const room of level.rooms) {
+        const roomGroup = group(parent)
+        translate(roomGroup, room.x * roomSpacing, room.y * roomSpacing)
+        hideWhenTaken.push({
+          hide: roomGroup,
+          distance: distanceSquared(room.x, room.y, mcguffinX, mcguffinY),
+        })
+
+        switch (room.type) {
+          case `empty`: {
+            sprite(roomGroup, game_room_empty_svg)
+          } break
+          case `mcguffin`:
+            sprite(roomGroup, game_room_mcguffin_a_svg)
+            break
+          case `switch`:
+            changeOnSwitch.push({
+              parent: roomGroup,
+              hide: sprite(
+                roomGroup,
+                switchState ? game_room_switch_a_svg : game_room_switch_b_svg
+              ),
+              show: switchState ? game_room_switch_b_svg : game_room_switch_a_svg,
+            })
+            break
+          default:
+            throw null
+        }
+      }
+
+      for (const corridor of level.corridors) {
+        const corridorGroup = group(parent)
+        translate(corridorGroup, corridor.x * roomSpacing, corridor.y * roomSpacing)
+        rotate(corridorGroup, facingDegrees[corridor.facing])
+        hideWhenTaken.push({
+          hide: corridorGroup,
+          distance: Math.min(
+            distanceSquared(corridor.x, corridor.y, mcguffinX, mcguffinY),
+            distanceSquared(corridor.x + facingX[corridor.facing], corridor.y + facingY[corridor.facing], mcguffinX, mcguffinY)
+          )
+        })
+
+        switch (corridor.type) {
+          case `empty`:
+            sprite(corridorGroup, game_corridor_empty_svg)
+            break
+          case `ledge`:
+            sprite(corridorGroup, game_corridor_ledge_svg)
+            break
+          case `stairs`:
+            sprite(corridorGroup, game_corridor_stairs_svg)
+            break
+          case `openDoor`:
+            changeOnSwitch.push({
+              parent: corridorGroup,
+              hide: sprite(
+                corridorGroup,
+                switchState ? game_corridor_door_open_svg : game_corridor_door_closed_svg
+              ),
+              show: switchState ? game_corridor_door_closed_svg : game_corridor_door_open_svg,
+            })
+            break
+          case `closedDoor`:
+            changeOnSwitch.push({
+              parent: corridorGroup,
+              hide: sprite(
+                corridorGroup,
+                switchState ? game_corridor_door_closed_svg : game_corridor_door_open_svg
+              ),
+              show: switchState ? game_corridor_door_open_svg : game_corridor_door_closed_svg,
+            })
+            break
+          case `goal`:
+            sprite(corridorGroup, game_corridor_goal_closed_svg)
+            break
+          default:
+            throw null
+        }
+      }
+    }
+
+    const playerGroup = group(parent)
+    translate(playerGroup, mode.x * roomSpacing, mode.y * roomSpacing)
+    rotate(playerGroup, facingDegrees[mode.facing])
+
+    const playerIdleA = sprite(playerGroup, game_player_idle_a_lit_svg)
+    const playerIdleB = sprite(playerGroup, game_player_idle_b_lit_svg)
+    const playerWalk = sprite(playerGroup, game_player_walk_lit_svg)
+
+    hide(playerIdleB)
+
     if (mode.animation) {
-      linear(playerGroup)
-      for (let i = 0; i < 8; i++) {
-        elapse(50)
-        scaleY(playerWalk, -1)
-      }
-      translateX(playerGroup, roomSpacing)
-      hide(playerWalk)
-      show(playerIdleA)
-    }
-
-    switch (mode.animation) {
-      case `take`:
-        while (hideWhenTaken.length) {
-          let shortestDistance = Infinity
-          for (const item of hideWhenTaken) {
-            if (item.distance < shortestDistance) {
-              shortestDistance = item.distance
-            }
-          }
-          for (let index = 0; index < hideWhenTaken.length;) {
-            const item = hideWhenTaken[index]
-            if (item.distance == shortestDistance) {
-              hide(item.hide)
-              hideWhenTaken.splice(index, 1)
-            } else {
-              index++
-            }
-          }
-          elapse(300)
-        }
-        break
-
-      case `switch`:
-        for (const change of changeOnSwitch) {
-          hide(change.hide)
-          sprite(change.parent, change.show)
-        }
-        break
-    }
-
-    if (mode.state == `won`) {
-      hide(playerGroup)
-      renderNonInteractiveMenu(parent, postGameMenu(mode))
-      phase()
-    } else {
-      for (const key of keys) {
-        const keyGroup = group(parent)
-        translate(
-          keyGroup,
-          halfSafeAreaWidthVirtualPixels + key.x * 32 + 16,
-          halfSafeAreaHeightVirtualPixels + key.y * 32 + 16
-        )
-        sprite(keyGroup, game_hud_key_svg)
-        write(keyGroup, key.text)
-      }
-
-      phase()
-
-      elapse(500)
       hide(playerIdleA)
-      show(playerIdleB)
-      elapse(500)
+      translateX(playerGroup, -roomSpacing)
+    } else {
+      hide(playerWalk)
+    }
+
+    return () => {
+      if (mode.animation) {
+        linear(playerGroup)
+        for (let i = 0; i < 8; i++) {
+          elapse(50)
+          scaleY(playerWalk, -1)
+        }
+        translateX(playerGroup, roomSpacing)
+        hide(playerWalk)
+        show(playerIdleA)
+      }
+
+      switch (mode.animation) {
+        case `take`:
+          while (hideWhenTaken.length) {
+            let shortestDistance = Infinity
+            for (const item of hideWhenTaken) {
+              if (item.distance < shortestDistance) {
+                shortestDistance = item.distance
+              }
+            }
+            for (let index = 0; index < hideWhenTaken.length;) {
+              const item = hideWhenTaken[index]
+              if (item.distance == shortestDistance) {
+                hide(item.hide)
+                hideWhenTaken.splice(index, 1)
+              } else {
+                index++
+              }
+            }
+            elapse(300)
+          }
+          break
+
+        case `switch`:
+          for (const change of changeOnSwitch) {
+            hide(change.hide)
+            sprite(change.parent, change.show)
+          }
+          break
+      }
+
+      if (mode.state == `won`) {
+        hide(playerGroup)
+        renderNonInteractiveMenu(parent, postGameMenu(mode), true)
+        phase()
+      } else {
+        for (const key of keys) {
+          const keyGroup = group(parent)
+          translate(
+            keyGroup,
+            halfSafeAreaWidthVirtualPixels + key.x * 32 + 16,
+            halfSafeAreaHeightVirtualPixels + key.y * 32 + 16
+          )
+          sprite(keyGroup, game_hud_key_svg)
+          write(keyGroup, key.text)
+        }
+
+        phase()
+
+        elapse(500)
+        hide(playerIdleA)
+        show(playerIdleB)
+        elapse(500)
+      }
     }
   }
 }
@@ -340,11 +345,11 @@ function renderInteractiveGame(
                 break
               }
             }
-                mode.animation = `walk`
-                return
-              }
-            }
+            mode.animation = `walk`
+            return
           }
         }
       }
     }
+  }
+}
