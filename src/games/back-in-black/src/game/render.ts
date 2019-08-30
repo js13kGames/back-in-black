d897aef6962cf5f3e137f0bb66a50b1ad92519a6
jameswilddev <jameswilddev@outlook.com>
2019-08-30 19:16:29 +0100

    Slight refactor of level rendering (#60).

--- src/games/back-in-black/src/game/render.ts
+++ src/games/back-in-black/src/game/render.ts
@@ -59,6 +59,63 @@ function postGameMenu(
   }
 }
 
+function renderStaticRooms(
+  parent: EngineViewport | EngineAnimation,
+  mode: GameMode,
+  level: Level,
+): void {
+  for (const room of level.rooms) {
+    translate(sprite(parent, game_room_empty_svg), room[0] * roomSpacing, room[1] * roomSpacing)
+  }
+  for (const room of level.switches) {
+    translate(sprite(parent, mode.switch == `a` ? game_room_switch_a_svg : game_room_switch_b_svg), room[0] * roomSpacing, room[1] * roomSpacing)
+  }
+}
+
+function renderCorridors(
+  parent: EngineViewport | EngineAnimation,
+  mode: GameMode,
+  level: Level,
+): void {
+  for (const corridor of level.corridors) {
+    let svg: EngineSpritesSvg
+
+    switch (corridor.type) {
+      case `empty`:
+        svg = game_corridor_empty_svg
+        break
+
+      case `ledge`:
+        svg = game_corridor_ledge_svg
+        break
+
+      case `stairs`:
+        svg = game_corridor_stairs_svg
+        break
+
+      case `openDoor`:
+        svg = mode.switch == `a` ? game_corridor_door_open_svg : game_corridor_door_closed_svg
+        break
+
+      case `closedDoor`:
+        svg = mode.switch == `a` ? game_corridor_door_closed_svg : game_corridor_door_open_svg
+        break
+
+      case `goal`:
+        svg = game_corridor_goal_closed_svg
+        break
+
+      default:
+        // TODO: this is impossible.
+        throw null
+    }
+
+    const corridorSprite = sprite(parent, svg)
+    translate(corridorSprite, corridor.x * roomSpacing, corridor.y * roomSpacing)
+    rotate(corridorSprite, facingDegrees[corridor.facing])
+  }
+}
+
 function renderNonInteractiveGame(
   parent: EngineViewport | EngineAnimation,
   mode: GameMode,
@@ -66,54 +123,13 @@ function renderNonInteractiveGame(
   const level = levels[mode.level]
   switch (mode.state) {
     case `initial`:
-      for (const room of level.rooms) {
-        translate(sprite(parent, game_room_empty_svg), room[0] * roomSpacing, room[1] * roomSpacing)
-      }
-      for (const room of level.switches) {
-        translate(sprite(parent, mode.switch == `a` ? game_room_switch_a_svg : game_room_switch_b_svg), room[0] * roomSpacing, room[1] * roomSpacing)
-      }
+      renderStaticRooms(parent, mode, level)
 
       const mcguffinGroup = group(parent)
       translate(mcguffinGroup, level.mcguffin[0] * roomSpacing, level.mcguffin[1] * roomSpacing)
       const mcguffinA = sprite(mcguffinGroup, game_room_mcguffin_a_svg)
 
-      for (const corridor of level.corridors) {
-        let svg: EngineSpritesSvg
-
-        switch (corridor.type) {
-          case `empty`:
-            svg = game_corridor_empty_svg
-            break
-
-          case `ledge`:
-            svg = game_corridor_ledge_svg
-            break
-
-          case `stairs`:
-            svg = game_corridor_stairs_svg
-            break
-
-          case `openDoor`:
-            svg = mode.switch == `a` ? game_corridor_door_open_svg : game_corridor_door_closed_svg
-            break
-
-          case `closedDoor`:
-            svg = mode.switch == `a` ? game_corridor_door_closed_svg : game_corridor_door_open_svg
-            break
-
-          case `goal`:
-            svg = game_corridor_goal_closed_svg
-            break
-
-          default:
-            // TODO: this is impossible.
-            throw null
-        }
-
-        const corridorSprite = sprite(parent, svg)
-        translate(corridorSprite, corridor.x * roomSpacing, corridor.y * roomSpacing)
-        rotate(corridorSprite, facingDegrees[corridor.facing])
-      }
+      renderCorridors(parent, mode, level)
 
       const playerGroup = group(parent)
       translate(playerGroup, mode.x * roomSpacing, mode.y * roomSpacing)
