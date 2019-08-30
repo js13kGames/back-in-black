c91d9d0152a59343c6b4f295b99292c30afa39b6
jameswilddev <jameswilddev@outlook.com>
2019-08-30 20:04:28 +0100

    Refactored corridor type (#60).

--- src/games/back-in-black/src/game/levels/type.ts
+++ src/games/back-in-black/src/game/levels/type.ts
@@ -1,12 +1,14 @@
+type Corridor = {
+  readonly type: `empty` | `ledge` | `stairs` | `openDoor` | `closedDoor` | `goal`
+  readonly x: number
+  readonly y: number
+  readonly facing: Facing
+}
+
 type Level = {
   readonly name: string
   readonly mcguffin: readonly [number, number]
   readonly switches: ReadonlyArray<readonly [number, number]>
   readonly rooms: ReadonlyArray<readonly [number, number]>
-  readonly corridors: ReadonlyArray<{
-    readonly type: `empty` | `ledge` | `stairs` | `openDoor` | `closedDoor` | `goal`
-    readonly x: number
-    readonly y: number
-    readonly facing: Facing
-  }>
+  readonly corridors: ReadonlyArray<Corridor>
 }
