bfb093ab0a4d76741886e03facd1b52fed70f2bf
jameswilddev <jameswilddev@outlook.com>
2019-08-30 19:07:31 +0100

    Reimplemented basic level rendering (#60).

--- src/games/back-in-black/src/game/render.ts
+++ src/games/back-in-black/src/game/render.ts
@@ -63,9 +63,72 @@ function renderNonInteractiveGame(
   parent: EngineViewport | EngineAnimation,
   mode: GameMode,
 ): () => (undefined | (() => void)) {
-  parent
-  mode
-  return () => { return undefined }
+  const level = levels[mode.level]
+  switch (mode.state) {
+    case `initial`:
+      for (const room of level.rooms) {
+        translate(sprite(parent, game_room_empty_svg), room[0] * roomSpacing, room[1] * roomSpacing)
+      }
+      for (const room of level.switches) {
+        translate(sprite(parent, mode.switch == `a` ? game_room_switch_a_svg : game_room_switch_b_svg), room[0] * roomSpacing, room[1] * roomSpacing)
+      }
+
+      const mcguffinGroup = group(parent)
+      translate(mcguffinGroup, level.mcguffin[0] * roomSpacing, level.mcguffin[1] * roomSpacing)
+      const mcguffinA = sprite(mcguffinGroup, game_room_mcguffin_a_svg)
+
+      for (const corridor of level.corridors) {
+        let svg: EngineSpritesSvg
+
+        switch (corridor.type) {
+          case `empty`:
+            svg = game_corridor_empty_svg
+            break
+
+          case `ledge`:
+            svg = game_corridor_ledge_svg
+            break
+
+          case `stairs`:
+            svg = game_corridor_stairs_svg
+            break
+
+          case `openDoor`:
+            svg = mode.switch == `a` ? game_corridor_door_open_svg : game_corridor_door_closed_svg
+            break
+
+          case `closedDoor`:
+            svg = mode.switch == `a` ? game_corridor_door_closed_svg : game_corridor_door_open_svg
+            break
+
+          case `goal`:
+            svg = game_corridor_goal_closed_svg
+            break
+
+          default:
+            // TODO: this is impossible.
+            throw null
+        }
+
+        const corridorSprite = sprite(parent, svg)
+        translate(corridorSprite, corridor.x * roomSpacing, corridor.y * roomSpacing)
+        rotate(corridorSprite, facingDegrees[corridor.facing])
+      }
+
+      return () => {
+        elapse(333)
+        hide(mcguffinA)
+        const mcguffinB = sprite(mcguffinGroup, game_room_mcguffin_b_svg)
+        elapse(333)
+        hide(mcguffinB)
+        sprite(mcguffinGroup, game_room_mcguffin_c_svg)
+        elapse(333)
+        return undefined
+      }
+
+    default:
+      return () => { return undefined }
+  }
 }
 
 function renderInteractiveGame(
