9b95f223427bbd2e4736624a942a1950d1bd7125
jameswilddev <jameswilddev@outlook.com>
2019-08-31 00:00:28 +0100

    Saved space by layering font characters.

--- src/games/back-in-black/src/font/write.ts
+++ src/games/back-in-black/src/font/write.ts
@@ -4,6 +4,8 @@ function write(
 ): void {
   let x = -7.5 * (text.length - 1)
   for (let i = 0; i < text.length; i++) {
+    const baseSprite = sprite(parent, font_base_svg)
+    translateX(baseSprite, x)
     const character = text.charAt(i)
     if (character != ` `) {
       const characterSprite = sprite(parent, font[character])
